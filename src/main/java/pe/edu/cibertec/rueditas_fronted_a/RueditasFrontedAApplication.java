d8db6ccb1073e6ede5ef28552189c4117a381c07
mileidylaura <mileidylaura150@gmail.com>
2024-09-25 17:04:37 -0500

    Creacion del nuevo Fronted-a en Rueditas

---
+++ src/main/java/pe/edu/cibertec/rueditas_fronted_a/RueditasFrontedAApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.rueditas_fronted_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RueditasFrontedAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RueditasFrontedAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/rueditas_fronted_a/RueditasFrontedAApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.rueditas_fronted_a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RueditasFrontedAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
